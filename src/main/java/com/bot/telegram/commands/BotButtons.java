20ed3f73bc6567fa6123c2594e13be412ad0db7f
Ashlander <ashlender1830@gmail.com>
2024-04-02 00:04:27 +0500

    full remake part 3.

--- src/main/java/com/bot/telegram/commands/BotButtons.java
+++ src/main/java/com/bot/telegram/commands/BotButtons.java
@@ -5,18 +5,31 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class BotButtons {
     public static InlineKeyboardMarkup keyboardMarkup(){
-        List<List<InlineKeyboardButton>> keyboardButton = new ArrayList<>();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        //row.add(new InlineKeyboardButton().setText("Button One").setCallbackData("button1"));
-       // row.add(new InlineKeyboardButton().setText("Кнопка 2").setCallbackData("button2"));
-        keyboardButton.add(row);
+        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
+        // список строк
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
 
-        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        markup.setKeyboard(keyboardButton);
+        //  первая строка с кнопкой
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Кнопка 1");
+        button1.setCallbackData("button1");
+        row1.add(button1);
 
-        return markup;
+        // вторая строка с кнопкой
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Кнопка 2");
+        button2.setCallbackData("button2");
+        row2.add(button2);
+
+        keyboard.add(row1);
+        keyboard.add(row2);
+
+        // созданные строки
+        markupKeyboard.setKeyboard(keyboard);
+        return markupKeyboard;
     }
 }
